dcc82158695633b43b4ba297468d2313ea310029
TetsuoOhtaguchi <ohtaguchi1202@gmail.com>
2024-01-23 08:10:31 +0900

    firebase初期化 完了

---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+import { FirebaseApp, initializeApp } from "firebase/app";
+import { Auth, getAuth } from "firebase/auth";
+import { Firestore, getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+}
+
+const app: FirebaseApp = initializeApp(firebaseConfig);
+export const auth: Auth = getAuth();
+export const db: Firestore = getFirestore(app);
