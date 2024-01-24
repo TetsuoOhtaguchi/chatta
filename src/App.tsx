a763e2ca442aecf3ed8f84e551173495abd47939
TetsuoOhtaguchi <ohtaguchi1202@gmail.com>
2024-01-25 08:45:11 +0900

    css emotion 導入完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 // import { signInWithEmailAndPassword, User, UserCredential } from "firebase/auth"
 // import { auth } from "./firebase"
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react"
+
+const title = css`
+  color: red;
+`
 
 const App = () => {
   // signInWithEmailAndPassword(auth, "example@gmail.com", "exampleuser").then(
@@ -10,7 +16,7 @@ const App = () => {
   //     }
   //   }
   // )
-  return <div>App</div>
+  return <div css={title}>App</div>
 }
 
 export default App
