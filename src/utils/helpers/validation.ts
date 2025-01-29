c01b885e1da0c45854bf39b6b0e6b2b3f4dc0fcd
TetsuoOhtaguchi <ohtaguchi1202@gmail.com>
2025-01-29 19:50:19 +0900

    chatta nameの登録処理を追加する #42
    - バリデーションチェックの修正を行う
      - 修正完了
      - chatta nameを半角英数字で入力チェックする
      - first name、last nameを半角英字で入力チェックする
    
    https://github.com/TetsuoOhtaguchi/chatta/issues/42

--- src/utils/helpers/validation.ts
+++ src/utils/helpers/validation.ts
@@ -9,11 +9,6 @@ interface UserDataType {
   password: string
 }
 
-// 名前のバリデーションパターン：全角ひらがな、全角漢字、全角カタカナ、全角英語、半角英語
-// todo バリデーションチェックのパターン修正を行う
-const nameValidPattern =
-  /^[\u30a0-\u30ff\u3040-\u309f\u3005-\u3006\u30e0-\u9fcf]+$/
-
 export const validationCheck = (
   procedure: ProcedureType,
   userData: UserDataType
@@ -34,11 +29,33 @@ export const validationCheck = (
     }
   }
 
+  // サインアップ時、チャッタネームに半角英数字意外が入力されている場合
+  if (
+    procedure === 'signup' &&
+    !userData.chattaName.trim().match(/^[A-Za-z0-9]*$/)
+  ) {
+    return {
+      errorCode: 'chattaNameError',
+      errorMessage: 'Please enter single-byte alphanumeric characters.'
+    }
+  }
+
   // サインアップ時、名が未入力の場合
   if (procedure === 'signup' && !userData.firstName) {
     return {
       errorCode: 'firstNameError',
       errorMessage: 'Please enter your first name.'
+    }
+  }
+
+  // サインアップ時、名に半角英字意外が入力されている場合
+  if (
+    procedure === 'signup' &&
+    !userData.firstName.trim().match(/^[A-Za-z]*$/)
+  ) {
+    return {
+      errorCode: 'firstNameError',
+      errorMessage: 'Contains invalid characters.'
     }
   }
 
@@ -50,32 +67,10 @@ export const validationCheck = (
     }
   }
 
-  // サインアップ時、チャッタネームに名前のバリデーションパターン以外の文字が入力されている場合
+  // サインアップ時、姓に半角英字意外が入力されている場合
   if (
     procedure === 'signup' &&
-    !nameValidPattern.test(userData.chattaName.trim())
-  ) {
-    return {
-      errorCode: 'chattaNameError',
-      errorMessage: 'Contains invalid characters.'
-    }
-  }
-
-  // サインアップ時、名に名前のバリデーションパターン以外の文字が入力されている場合
-  if (
-    procedure === 'signup' &&
-    !nameValidPattern.test(userData.firstName.trim())
-  ) {
-    return {
-      errorCode: 'firstNameError',
-      errorMessage: 'Contains invalid characters.'
-    }
-  }
-
-  // サインアップ時、姓に名前のバリデーションパターン以外の文字が入力されている場合
-  if (
-    procedure === 'signup' &&
-    !nameValidPattern.test(userData.lastName.trim())
+    !userData.lastName.trim().match(/^[A-Za-z]*$/)
   ) {
     return {
       errorCode: 'lastNameError',
